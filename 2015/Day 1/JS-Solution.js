ff4f73c4938b17efefb6236a4ca7604fd501fd8d
Mohamed El Mahdali <mohamedelmahdali11@gmail.com>
2018-02-26 22:51:41 +0000

    Add part2 solution to 2015/Day1 in JS and Python

--- 2015/Day 1/JS-Solution.js
+++ 2015/Day 1/JS-Solution.js
@@ -1,4 +1,6 @@
-whatFloor = (inputText) => {
+//Solution for Part 1
+
+whatFloor1 = (inputText) => {
 	inputText = inputText.split('');
 	floor = 0;
 	for(let char of inputText){
@@ -16,7 +18,7 @@ whatFloor = (inputText) => {
 //This one is less readable in my opinion, but it's shorter.
 
 /*
-whatFloor = (inputText) => {
+whatFloor1 = (inputText) => {
 	inputText = inputText.split('');
 	goUp = (inputText.filter(char => char == '(')).length;
 	goDown = inputText.length - goUp;
@@ -25,3 +27,21 @@ whatFloor = (inputText) => {
 	return floor;
 };
 */
+
+
+
+//Solution for Part 2
+
+causedTogoIntoBasement = (inputText) => {
+	inputText = inputText.split('');
+	floor = 0;
+	for(let i in inputText){
+	  if (inputText[i] == '('){
+	    floor += 1;
+	  }
+	  else{
+	    floor -= 1;
+	  }
+	  if(floor == -1) return parseInt(i) + 1;
+	}
+};
